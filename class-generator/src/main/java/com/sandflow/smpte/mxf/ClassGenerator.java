5a5ab598507e104e95d868531db7451265e10a8d
Pierre-Anthony Lemieux <pal@palemieux.com>
2025-04-18 18:21:51 +0200

    Making progess on type generation

--- class-generator/src/main/java/com/sandflow/smpte/mxf/ClassGenerator.java
+++ class-generator/src/main/java/com/sandflow/smpte/mxf/ClassGenerator.java
@@ -3,34 +3,359 @@ package com.sandflow.smpte.mxf;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 
 import com.github.jknack.handlebars.Handlebars;
 import com.github.jknack.handlebars.Template;
+import com.sandflow.smpte.regxml.dict.DefinitionResolver;
 import com.sandflow.smpte.regxml.dict.MetaDictionaryCollection;
+import com.sandflow.smpte.regxml.dict.definitions.CharacterTypeDefinition;
 import com.sandflow.smpte.regxml.dict.definitions.ClassDefinition;
 import com.sandflow.smpte.regxml.dict.definitions.Definition;
+import com.sandflow.smpte.regxml.dict.definitions.DefinitionVisitor.VisitorException;
+import com.sandflow.smpte.regxml.dict.definitions.EnumerationTypeDefinition;
+import com.sandflow.smpte.regxml.dict.definitions.ExtendibleEnumerationTypeDefinition;
+import com.sandflow.smpte.regxml.dict.definitions.FixedArrayTypeDefinition;
+import com.sandflow.smpte.regxml.dict.definitions.FloatTypeDefinition;
+import com.sandflow.smpte.regxml.dict.definitions.IndirectTypeDefinition;
+import com.sandflow.smpte.regxml.dict.definitions.IntegerTypeDefinition;
+import com.sandflow.smpte.regxml.dict.definitions.LensSerialFloatTypeDefinition;
+import com.sandflow.smpte.regxml.dict.definitions.NullDefinitionVisitor;
+import com.sandflow.smpte.regxml.dict.definitions.OpaqueTypeDefinition;
+import com.sandflow.smpte.regxml.dict.definitions.PropertyAliasDefinition;
 import com.sandflow.smpte.regxml.dict.definitions.PropertyDefinition;
+import com.sandflow.smpte.regxml.dict.definitions.RecordTypeDefinition;
+import com.sandflow.smpte.regxml.dict.definitions.RenameTypeDefinition;
+import com.sandflow.smpte.regxml.dict.definitions.SetTypeDefinition;
+import com.sandflow.smpte.regxml.dict.definitions.StreamTypeDefinition;
+import com.sandflow.smpte.regxml.dict.definitions.StringTypeDefinition;
+import com.sandflow.smpte.regxml.dict.definitions.StrongReferenceTypeDefinition;
+import com.sandflow.smpte.regxml.dict.definitions.VariableArrayTypeDefinition;
+import com.sandflow.smpte.regxml.dict.definitions.WeakReferenceTypeDefinition;
 import com.sandflow.smpte.util.AUID;
+import com.sandflow.smpte.util.UL;
 
 public class ClassGenerator {
   public static final Handlebars handlebars = new Handlebars();
   public static final Template classTemplate;
+  public static final Template enumerationTemplate;
+  public static final Template variableArrayTemplate;
 
   static {
     try {
-      classTemplate = handlebars.compile("hbs/MXFClass.java");
+      classTemplate = handlebars.compile("hbs/Class.java");
+      enumerationTemplate = handlebars.compile("hbs/Enumeration.java");
+      variableArrayTemplate = handlebars.compile("hbs/VariableArrayAdapter.java");
     } catch (Exception e) {
       throw new RuntimeException("Failed to load template", e);
     }
   }
 
-  public static void generate(MetaDictionaryCollection mds, File generatedSourcesDir)
-      throws IOException, URISyntaxException {
+  class TypeMaker extends NullDefinitionVisitor {
+    private static final UL INSTANCE_UID_ITEM_UL = UL.fromURN("urn:smpte:ul:060e2b34.01010101.01011502.00000000");
+    private static final UL AUID_UL = UL.fromDotValue("06.0E.2B.34.01.04.01.01.01.03.01.00.00.00.00.00");
+    private static final UL UUID_UL = UL.fromDotValue("06.0E.2B.34.01.04.01.01.01.03.03.00.00.00.00.00");
+    private static final UL DateStruct_UL = UL.fromDotValue("06.0E.2B.34.01.04.01.01.03.01.05.00.00.00.00.00");
+    private static final UL PackageID_UL = UL.fromDotValue("06.0E.2B.34.01.04.01.01.01.03.02.00.00.00.00.00");
+    private static final UL Rational_UL = UL.fromDotValue("06.0E.2B.34.01.04.01.01.03.01.01.00.00.00.00.00");
+    private static final UL TimeStruct_UL = UL.fromDotValue("06.0E.2B.34.01.04.01.01.03.01.06.00.00.00.00.00");
+    private static final UL TimeStamp_UL = UL.fromDotValue("06.0E.2B.34.01.04.01.01.03.01.07.00.00.00.00.00");
+    private static final UL VersionType_UL = UL.fromDotValue("06.0E.2B.34.01.04.01.01.03.01.03.00.00.00.00.00");
+    private static final UL ByteOrder_UL = UL.fromDotValue("06.0E.2B.34.01.01.01.01.03.01.02.01.02.00.00.00");
+    private static final UL Character_UL = UL.fromURN("urn:smpte:ul:060e2b34.01040101.01100100.00000000");
+    private static final UL Char_UL = UL.fromURN("urn:smpte:ul:060e2b34.01040101.01100300.00000000");
+    private static final UL UTF8Character_UL = UL.fromURN("urn:smpte:ul:060e2b34.01040101.01100500.00000000");
+    private static final UL ProductReleaseType_UL = UL.fromURN("urn:smpte:ul:060e2b34.01040101.02010101.00000000");
+    private static final UL Boolean_UL = UL.fromURN("urn:smpte:ul:060e2b34.01040101.01040100.00000000");
+    private static final UL PrimaryPackage_UL = UL.fromURN("urn:smpte:ul:060e2b34.01010104.06010104.01080000");
+    private static final UL LinkedGenerationID_UL = UL.fromURN("urn:smpte:ul:060e2b34.01010102.05200701.08000000");
+    private static final UL GenerationID_UL = UL.fromURN("urn:smpte:ul:060e2b34.01010102.05200701.01000000");
+    private static final UL ApplicationProductID_UL = UL.fromURN("urn:smpte:ul:060e2b34.01010102.05200701.07000000");
+    private static final AUID AUID_AUID = new AUID(UL.fromDotValue("06.0E.2B.34.01.04.01.01.01.03.01.00.00.00.00.00"));
+    private static final AUID UUID_AUID = new AUID(UL.fromDotValue("06.0E.2B.34.01.04.01.01.01.03.03.00.00.00.00.00"));
 
-    HashMap<AUID, String> typeNames = new HashMap<>();
+    boolean isNullable;
+
+    String typeName;
+    String adapterName;
+
+    public String getTypeName() {
+      return typeName;
+    }
+
+    public String getAdapterName() {
+      return adapterName;
+    }
+
+    private TypeMaker(boolean isNullabe) {
+      this.isNullable = isNullabe;
+    }
+
+    @Override
+    public void visit(ClassDefinition def) throws VisitorException {
+      this.typeName = def.getSymbol();
+      this.adapterName = "ClassAdapter";
+    }
+
+    @Override
+    public void visit(CharacterTypeDefinition def) throws VisitorException {
+      this.typeName = "String";
+
+      if (def.getIdentification().equals(Character_UL)) {
+        this.adapterName = "UTF16Adapter";
+      } else if (def.getIdentification().equals(Char_UL)) {
+        this.adapterName = "USASCIIAdapter";
+      } else if (def.getIdentification().equals(UTF8Character_UL)) {
+        this.adapterName = "UTF8Adapter";
+      } else {
+        throw new VisitorException("Unknown character type " + def.getIdentification());
+      }
+    }
+
+    @Override
+    public void visit(IntegerTypeDefinition def) throws VisitorException {
+      if (def.isSigned()) {
+        switch (def.getSize()) {
+          case ONE:
+            this.typeName = this.isNullable ? "Byte" : "byte";
+            this.adapterName = "Int8Adapter";
+            break;
+          case TWO:
+            this.typeName = this.isNullable ? "Short" : "short";
+            this.adapterName = "Int16Adapter";
+            break;
+          case FOUR:
+            this.typeName = this.isNullable ? "Integer" : "int";
+            this.adapterName = "Int32Adapter";
+            break;
+          case EIGHT:
+            this.typeName = this.isNullable ? "Long" : "long";
+            this.adapterName = "Int64Adapter";
+            break;
+          default:
+            throw new VisitorException("Unknown integer type " + def.getIdentification());
+        }
+      } else {
+        switch (def.getSize()) {
+          case ONE:
+            this.typeName = this.isNullable ? "Short" : "short";
+            this.adapterName = "UInt8Adapter";
+            break;
+          case TWO:
+            this.typeName = this.isNullable ? "Integer" : "int";
+            this.adapterName = "UInt16Adapter";
+            break;
+          case FOUR:
+            this.typeName = this.isNullable ? "Long" : "long";
+            this.adapterName = "UInt32Adapter";
+            break;
+          case EIGHT:
+            this.typeName = this.isNullable ? "Long" : "long";
+            this.adapterName = "UInt64Adapter";
+            break;
+          default:
+            throw new VisitorException("Unknown integer type " + def.getIdentification());
+        }
+      }
+    }
+
+    @Override
+    public void visit(ExtendibleEnumerationTypeDefinition def) throws VisitorException {
+      this.typeName = "com.sandflow.smpte.util.UL";
+      this.adapterName = "ULAdapter";
+    }
+
+    @Override
+    public void visit(EnumerationTypeDefinition def) throws VisitorException {
+      var templateData = new HashMap<String, Object>();
+      templateData.put("symbol", def.getSymbol());
+      templateData.put("elementTypeName", "int");
+
+      var valuesData = new ArrayList<HashMap<String, String>>();
+      templateData.put("values", valuesData);
+
+      for (var value : def.getElements()) {
+        var valueData = new HashMap<String, String>();
+        valueData.put("name", value.getName());
+        valueData.put("value", Integer.toString(value.getValue()));
+        valuesData.add(valueData);
+      }
+
+      generateSource(enumerationTemplate, def.getSymbol(), templateData);
+
+      this.adapterName = "EnumerationAdapter";
+    }
+
+    @Override
+    public void visit(FixedArrayTypeDefinition def) throws VisitorException {
+      var templateData = new HashMap<String, Object>();
+
+      String adapterName = def.getSymbol() + "Adapter";
+      templateData.put("adapterName", adapterName);
+      templateData.put("itemCount", def.getElementCount());
+
+      TypeMaker tm = getTypeInformation(resolver.getDefinition(def.getElementType()), false);
+      templateData.put("itemTypeName", tm.getTypeName());
+
+      generateSource(variableArrayTemplate, adapterName, templateData);
+
+      this.adapterName = adapterName;
+      this.typeName = tm.getTypeName() + "[]";
+    }
+
+    @Override
+    public void visit(IndirectTypeDefinition def) throws VisitorException {
+      // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void visit(OpaqueTypeDefinition def) throws VisitorException {
+      // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void visit(RecordTypeDefinition def) throws VisitorException {
+      // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void visit(RenameTypeDefinition def) throws VisitorException {
+      TypeMaker tm = getTypeInformation(resolver.getDefinition(def.getRenamedType()), this.isNullable);
+
+      this.typeName = tm.typeName;
+      this.adapterName = tm.adapterName;
+    }
+
+    @Override
+    public void visit(SetTypeDefinition def) throws VisitorException {
+      this.typeName = isNullable ? "Integer" : "int";
+      this.adapterName = "Int32Adapter";
+    }
+
+    @Override
+    public void visit(StreamTypeDefinition def) throws VisitorException {
+      this.typeName = isNullable ? "Integer" : "int";
+      this.adapterName = "Int32Adapter";
+    }
+
+    @Override
+    public void visit(StrongReferenceTypeDefinition def) throws VisitorException {
+      this.typeName = isNullable ? "Integer" : "int";
+      this.adapterName = "Int32Adapter";
+    }
+
+    @Override
+    public void visit(StringTypeDefinition def) throws VisitorException {
+      this.typeName = isNullable ? "Integer" : "int";
+      this.adapterName = "Int32Adapter";
+    }
+
+    @Override
+    public void visit(VariableArrayTypeDefinition def) throws VisitorException {
+      var templateData = new HashMap<String, Object>();
+
+      String adapterName = def.getSymbol() + "Adapter";
+      templateData.put("adapterName", adapterName);
+
+      Definition itemDef = resolver.getDefinition(def.getElementType());
+      templateData.put("itemTypeName", itemDef.getSymbol());
+
+      generateSource(variableArrayTemplate, adapterName, templateData);
+
+      this.adapterName = adapterName;
+      this.typeName = itemDef.getSymbol() + "[]";
+    }
+
+    @Override
+    public void visit(WeakReferenceTypeDefinition def) throws VisitorException {
+      ClassDefinition classdef = (ClassDefinition) resolver.getDefinition(def.getReferencedType());
+
+      PropertyDefinition uniquepropdef = null;
+
+      for (PropertyDefinition propdef : getAllMembersOf(classdef)) {
+
+        if (propdef.isUniqueIdentifier()) {
+          uniquepropdef = propdef;
+          break;
+        }
+      }
+
+      if (uniquepropdef == null) {
+        throw new VisitorException("WeakReferenceTypeDefinition " + def.getIdentification()
+            + " does not have a unique identifier");
+      }
+
+      TypeMaker tm = getTypeInformation(resolver.getDefinition(uniquepropdef.getType()), true);
+
+      this.typeName = tm.typeName;
+      this.adapterName = tm.adapterName;
+    }
+
+    @Override
+    public void visit(FloatTypeDefinition def) throws VisitorException {
+      this.typeName = isNullable ? "Integer" : "int";
+      this.adapterName = "Int32Adapter";
+    }
+
+    @Override
+    public void visit(LensSerialFloatTypeDefinition def) throws VisitorException {
+      this.typeName = isNullable ? "Integer" : "int";
+      this.adapterName = "Int32Adapter";
+    }
+
+  }
+
+  DefinitionResolver resolver;
+  final private HashMap<AUID, TypeMaker> typeCache = new HashMap<AUID, TypeMaker>();
+  final private HashMap<AUID, TypeMaker> nullableTypeCache = new HashMap<AUID, TypeMaker>();
+  File generatedSourcesDir;
+
+  private ClassGenerator(MetaDictionaryCollection mds, File generatedSourcesDir) {
+    this.resolver = mds;
+    this.generatedSourcesDir = generatedSourcesDir;
+  }
+
+  private TypeMaker getTypeInformation(Definition def, boolean isNullabe) throws VisitorException {
+    HashMap<AUID, TypeMaker> t = isNullabe ? nullableTypeCache : typeCache;
+    TypeMaker tm = t.get(def.getIdentification());
+    if (tm == null) {
+      tm = this.new TypeMaker(isNullabe);
+      def.accept(tm);
+      t.put(def.getIdentification(), tm);
+    }
+    return tm;
+  }
+
+  private Collection<PropertyDefinition> getAllMembersOf(ClassDefinition definition) {
+    ClassDefinition cdef = definition;
+
+    ArrayList<PropertyDefinition> props = new ArrayList<>();
+
+    while (cdef != null) {
+
+      for (AUID auid : resolver.getMembersOf(cdef)) {
+        props.add((PropertyDefinition) resolver.getDefinition(auid));
+      }
+
+      if (cdef.getParentClass() != null) {
+        cdef = (ClassDefinition) resolver.getDefinition(cdef.getParentClass());
+      } else {
+        cdef = null;
+      }
+
+    }
+
+    return props;
+  }
+
+  public static void generate(MetaDictionaryCollection mds, File generatedSourcesDir)
+      throws IOException, URISyntaxException, VisitorException {
+
+    ClassGenerator g = new ClassGenerator(mds, generatedSourcesDir);
 
     for (var md : mds.getDictionaries()) {
 
@@ -67,25 +392,14 @@ public class ClassGenerator {
             throw new RuntimeException("Failed to find property definition for " + propertyAUID);
           }
 
-          AUID typeAUID = propertyDef.getType();
+          Definition typeDef = g.resolver.getDefinition(propertyDef.getType());
 
-          String typeName = typeNames.get(typeAUID);
-
-          if (typeName == null) {
-
-            Definition typeDef = mds.getDefinition(typeAUID);
-            if (typeDef == null) {
-              throw new RuntimeException("Failed to find type definition for " + propertyDef.getType());
-            }
-
-            typeName = "int";
-
-          }
+          TypeMaker t = g.getTypeInformation(typeDef, true);
 
           var member = new HashMap<String, String>();
           member.put("identification", propertyDef.getIdentification().toString());
           member.put("type", propertyDef.getType().toString());
-          member.put("typeName", typeName);
+          member.put("typeName", t.getTypeName());
           member.put("symbol", propertyDef.getSymbol());
           member.put("localIdentification", Integer.toString(propertyDef.getLocalIdentification()));
           member.put("isOptional", propertyDef.isOptional() ? "true" : "false");
@@ -95,16 +409,20 @@ public class ClassGenerator {
 
         data.put("members", members);
 
-        try {
-          var classFile = new File(generatedSourcesDir, def.getSymbol() + ".java");
-          var os = new FileWriter(classFile);
-          os.write(classTemplate.apply(data));
-          os.close();
-        } catch (Exception e) {
-          throw new RuntimeException("Failed to write class file", e);
-        }
+        g.generateSource(classTemplate, classDef.getSymbol(), data);
 
       }
+    }
+  }
+
+  private void generateSource(Template template, String symbol, Object data) {
+    try {
+      var classFile = new File(generatedSourcesDir, symbol + ".java");
+      var os = new FileWriter(classFile);
+      os.write(template.apply(data));
+      os.close();
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to write class file", e);
     }
   }
 }
