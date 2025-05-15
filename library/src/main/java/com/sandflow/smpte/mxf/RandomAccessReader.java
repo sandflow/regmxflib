423cb0d74ddb11401f04f5e5f189b6f5dbc2ab9f
Pierre-Anthony Lemieux <pal@palemieux.com>
2025-05-15 13:34:41 -0700

    Started work on index tables

--- library/src/main/java/com/sandflow/smpte/mxf/RandomAccessReader.java
+++ library/src/main/java/com/sandflow/smpte/mxf/RandomAccessReader.java
@@ -31,6 +31,8 @@ import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.TreeMap;
 
 import org.apache.commons.numbers.fraction.Fraction;
 
@@ -40,36 +42,127 @@ import com.sandflow.smpte.klv.exceptions.KLVException;
 import com.sandflow.smpte.mxf.RandomIndexPack.PartitionOffset;
 
 public class RandomAccessReader extends StreamingReader {
-  public abstract class RandomAccessInputSource extends InputStream {
-      /**
-       * Set the position (in bytes) from the beginning of the source.
-       *
-       * @param pos Position in bytes
-       */
-    	public void position(long pos) {
-        throw new UnsupportedOperationException();
-      }
 
-      /**
-       * Returns the size (in bytes) of the source.
+  interface Index {
+    long getPos(int editUnit);
 
-       * @return Size in bytes
-       */
-      public long size() {
-        throw new UnsupportedOperationException();
-      }
-
-      /**
-       * Returns the position (in bytes) from the beginning of the source.
-
-       * @return Position in bytes
-       */
-      public long position() {
-        throw new UnsupportedOperationException();
-      }
+    long length();
   }
 
-  ArrayList<Long> vbeBytePositions;
+  class CBECLipIndex implements Index {
+    private long cbeSize;
+    private long startPos;
+    private long length;
+
+    CBECLipIndex(long cbeSize, long startPos, long length) {
+      if (length <= 0) {
+        throw new IllegalArgumentException();
+      }
+      if (startPos <= 0) {
+        throw new IllegalArgumentException();
+      }
+      if (length <= 0) {
+        throw new IllegalArgumentException();
+      }
+      this.cbeSize = cbeSize;
+      this.startPos = startPos;
+      this.length = length;
+    }
+
+    @Override
+    public long getPos(int editUnit) {
+      if (editUnit >= this.length) {
+        throw new IllegalArgumentException();
+      }
+      return this.startPos + this.cbeSize * editUnit;
+    }
+
+    @Override
+    public long length() {
+      return this.length;
+    }
+  }
+
+  class VBEIndex implements Index {
+    private ArrayList<Long> pos = new ArrayList<>();
+
+    void add(long pos) {
+      this.pos.add(pos);
+    }
+
+    @Override
+    public long getPos(int editUnit) {
+      if (editUnit >= this.pos.size()) {
+        throw new IllegalArgumentException();
+      }
+      return (long) this.pos.get(editUnit);
+    }
+
+    @Override
+    public long length() {
+      return this.pos.size();
+    }
+  }
+
+  public abstract class RandomAccessInputSource extends InputStream {
+    /**
+     * Set the position (in bytes) from the beginning of the source.
+     *
+     * @param pos Position in bytes
+     */
+    public void position(long pos) {
+      throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Returns the size (in bytes) of the source.
+     * 
+     * @return Size in bytes
+     */
+    public long size() {
+      throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Returns the position (in bytes) from the beginning of the source.
+     * 
+     * @return Position in bytes
+     */
+    public long position() {
+      throw new UnsupportedOperationException();
+    }
+  }
+
+  public abstract class RandomAccessInputSource extends InputStream {
+    /**
+     * Set the position (in bytes) from the beginning of the source.
+     *
+     * @param pos Position in bytes
+     */
+    public void position(long pos) {
+      throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Returns the size (in bytes) of the source.
+     * 
+     * @return Size in bytes
+     */
+    public long size() {
+      throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Returns the position (in bytes) from the beginning of the source.
+     * 
+     * @return Position in bytes
+     */
+    public long position() {
+      throw new UnsupportedOperationException();
+    }
+  }
+
+  HashMap<Long, Index> indexBySID;
   RandomAccessInputSource fis;
   MXFInputStream mis;
   RandomIndexPack rip;
@@ -104,9 +197,11 @@ public class RandomAccessReader extends StreamingReader {
 
     /* build index table and identify the right header metadata */
 
-    PartitionPack headerMetadataPartition;
+    PartitionPack headerMetadataPartition = null;
+    long lastIndexStart = 0;
 
     for (int i = 0; i < this.rip.getOffsets().size(); i++) {
+
       /* seek to and read partition */
       this.fis.position(this.rip.getOffsets().get(i).getOffset());
       t = mis.readTriplet();
@@ -114,37 +209,17 @@ public class RandomAccessReader extends StreamingReader {
 
       /* look for header metadata */
       if ((i == 0 || i == this.rip.getOffsets().size() - 1) &&
-          (pp.getStatus() == PartitionPack.Status.CLOSED_COMPLETE || pp.getStatus() == PartitionPack.Status.CLOSED_INCOMPLETE) &&
+          (pp.getStatus() == PartitionPack.Status.CLOSED_COMPLETE
+              || pp.getStatus() == PartitionPack.Status.CLOSED_INCOMPLETE)
+          &&
           pp.getHeaderByteCount() > 0) {
-            headerPartition = pp;
-      }
-    }
-
-    for (PartitionOffset po : this.rip.getOffsets()) {
-      this.fis.position(po.getOffset());
-
-      t = mis.readTriplet();
-      PartitionPack pp = PartitionPack.fromTriplet(t);
-
-      if (pp == null) {
-        continue;
+        headerMetadataPartition = pp;
       }
 
-            if (pp.getStatus() == PartitionPack.Status.CLOSED_COMPLETE) {
-        headerPP = pp;
-      } else if (pp.getStatus() == PartitionPack.Status.CLOSED_COMPLETE) {
-
+      /* look for an index table */
+      if (pp.getIndexSID() > 0 && pp.getIndexByteCount() > 0) {
+        t = mis.readTriplet();
       }
-
-      if (pp.getIndexSID() == 0 || pp.getIndexByteCount() == 0) {
-        continue;
-      }
-
-      if (pp.getIndexByteCount() > 0) {
-
-      }
-
-
     }
 
     /* VBE */
